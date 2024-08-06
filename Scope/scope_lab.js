3729a2cf90b98f0008be080ca147b1be9e932ffe
tacotones <tajackso5788@gmail.com>
2024-08-06 17:22:55 -0400

    practiceVar

--- Scope/scope_lab.js
+++ Scope/scope_lab.js
@@ -3,28 +3,21 @@ var globalVar = "I'm a global variable";
 let globalLet = "I'm also global, but scoped with let";
 const globalConst = "I'm a global constant";
 
-
 {
-// Block scope
-var blockVar = "I'm a block-scoped var";
-let blockLet = "I'm a block-scoped let";
-const blockConst = "I'm a block-scoped const";
+    // Second Block
+    var secondVar = "This is a var";
+    let secondLet = "This is a second let";
+    const secondConst = "This is a second const";
+
+    secondVar = "This is the updated second var";
+    secondLet = "this is an updated second let";
+    //secondConst = "this is an updated second const";
+
+    //console.log(secondVar);
+    console.log(secondLet);
+    console.log(secondConst);
 }
 
-// Global scope
-console.log(globalVar); // Output: "I'm a global variable"
-console.log(globalLet); // Output: "I'm also global, but scoped with let"
-console.log(globalConst); // Output: "I'm a global constant"
-//Block Scope
-//console.log(blockVar);
-//console.log(blockLet);
-function show(){
-    var functionVar = "I'm a block-scoped var";
-    let functionLet = "I'm a block-scoped let";
-    const functionConst = "I'm a block-scoped const";
-    }
-    show();
-    
-    console.log(functionVar); // Throws ReferenceError
-    console.log(functionLet); // Throws ReferenceError
-    console.log(functionConst); // Throws ReferenceError
+console.log(secondVar);
+console.log(secondLet);
+console.log(secondConst);
